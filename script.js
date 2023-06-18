070adfda381c2e900fe87daafeeb2dcc1b78ca31
Parimalayedida <21b01a1289@svecw.edu.in>
2023-06-19 04:55:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Add smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Handle form submission
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Perform form validation and submission logic here
+
+});
